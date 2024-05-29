313c09ce6ef69b223f7c7b45bbb543fc7cc824a1
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-05-29 11:42:04 +0500

    init

--- client/src/api/axios.api.ts
+++ client/src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://cs-timer-serv.onrender.com/api",
   headers: {
     Authorization: "Bearer " + getToken(),
   },
