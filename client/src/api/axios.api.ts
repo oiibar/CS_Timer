40056b7d7e3334c19d8bb981c29e0cb37277ad23
oiibar <95119839+oiibar@users.noreply.github.com>
2024-10-19 21:08:15 +0500

    Update axios.api.ts

--- client/src/api/axios.api.ts
+++ client/src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-  baseURL: "https://cs-timer-serv.onrender.com/api",
+  baseURL: "https://cstimer-production.up.railway.app/api",
   headers: {
     Authorization: "Bearer " + getToken(),
   },
