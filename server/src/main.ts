658d76c410d7bb894e04f09097fce91621c974ab
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2025-05-01 02:33:47 +0500

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://cs-timer-six.vercel.app',
+    credentials: true,
+  });
   await app.listen(10000, '0.0.0.0');
 }
 bootstrap();
